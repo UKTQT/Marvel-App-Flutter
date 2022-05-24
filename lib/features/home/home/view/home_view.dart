94e56d4451e020108f1a3ea69a6dfec8def9bc31
UKTQT <ufukkt2699@hotmail.com>
2022-05-24 17:48:02 +0300

    main dart update

--- lib/features/home/home/view/home_view.dart
+++ lib/features/home/home/view/home_view.dart
@@ -8,6 +8,7 @@ import '../../series/viewModel/series_view_model.dart';
 import '../../comic/viewModel/comic_view_model.dart';
 
 import '../../../../core/extensions/padding_extension/padding_extension.dart';
+import '../../../../core/extensions/color_extension/color_extension.dart';
 
 import 'home_character_box/home_character_box.dart';
 import 'home_comic_box/home_comic_box.dart';
@@ -28,119 +29,97 @@ class HomeView extends StatelessWidget {
         model.init();
       },
       onPageBuilder: (BuildContext context, HomeViewModel _homeViewModel) {
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: context.highHeightPadding2),
-              AspectRatio(
-                aspectRatio: 16 / 2,
-                child:
-                    homeSearch(context: context, homeViewModel: _homeViewModel),
-              ),
-              SizedBox(height: context.highHeightPadding2),
-              Observer(
-                builder: (_) {
-                  return _homeViewModel.searchItems.isNotEmpty
-                      ? const Text('ewq')
-                      : Column(
-                          children: [
-                            AspectRatio(
-                              aspectRatio: 16 / 10,
-                              child: homeCharacter(
-                                  context: context,
-                                  homeViewModel: _homeViewModel),
-                            ),
-                            SizedBox(height: context.mediumHeightPadding2),
-                            AspectRatio(
-                              aspectRatio: 16 / 10,
-                              child: homeComic(
-                                  context: context,
-                                  homeViewModel: _homeViewModel),
-                            ),
-                            SizedBox(height: context.mediumHeightPadding2),
-                            AspectRatio(
-                              aspectRatio: 16 / 9,
-                              child: homeSeries(
-                                  context: context,
-                                  homeViewModel: _homeViewModel),
-                            ),
-                          ],
-                        );
-                },
-              ),
-              SizedBox(height: context.highHeightPadding),
-            ],
+        return Scaffold(
+          appBar: appBarCustom(context),
+          //bottomNavigationBar: bottomNavBar(),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: context.highHeightPadding2),
+                AspectRatio(
+                  aspectRatio: 16 / 2,
+                  child: homeSearch(
+                      context: context, homeViewModel: _homeViewModel),
+                ),
+                SizedBox(height: context.highHeightPadding2),
+                Observer(
+                  builder: (_) {
+                    return _homeViewModel.searchItems.isNotEmpty
+                        ? const Text('ewq')
+                        : Column(
+                            children: [
+                              AspectRatio(
+                                aspectRatio: 16 / 10,
+                                child: homeCharacter(
+                                    context: context,
+                                    homeViewModel: _homeViewModel),
+                              ),
+                              SizedBox(height: context.mediumHeightPadding2),
+                              AspectRatio(
+                                aspectRatio: 16 / 10,
+                                child: homeComic(
+                                    context: context,
+                                    homeViewModel: _homeViewModel),
+                              ),
+                              SizedBox(height: context.mediumHeightPadding2),
+                              AspectRatio(
+                                aspectRatio: 16 / 9,
+                                child: homeSeries(
+                                    context: context,
+                                    homeViewModel: _homeViewModel),
+                              ),
+                            ],
+                          );
+                  },
+                ),
+                SizedBox(height: context.highHeightPadding),
+              ],
+            ),
           ),
         );
       },
     );
   }
-}
 
-
-
-
-
-
-
-
-/* class HomeView extends StatefulWidget {
-  const HomeView({Key? key}) : super(key: key);
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  final HomeViewModel _homeViewModel = HomeViewModel();
-
-  @override
-  Widget build(BuildContext context) {
-    return 
-  }
-} */
-
-/*
-SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(height: context.highHeightPadding2),
-          AspectRatio(
-            aspectRatio: 16 / 2,
-            child: homeSearch(context: context, homeViewModel: _homeViewModel),
-          ),
-          SizedBox(height: context.highHeightPadding2),
-          Observer(
-            builder: (_) {
-              return _homeViewModel.searchItems.isNotEmpty
-                  ? const Text('ewq')
-                  : Column(
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 16 / 10,
-                          child: homeCharacter(
-                              context: context, homeViewModel: _homeViewModel),
-                        ),
-                        SizedBox(height: context.mediumHeightPadding2),
-                        AspectRatio(
-                          aspectRatio: 16 / 10,
-                          child: homeComic(
-                              context: context, homeViewModel: _homeViewModel),
-                        ),
-                        SizedBox(height: context.mediumHeightPadding2),
-                        AspectRatio(
-                          aspectRatio: 16 / 9,
-                          child: homeSeries(
-                              context: context, homeViewModel: _homeViewModel),
-                        ),
-                      ],
-                    );
-            },
-          ),
-          SizedBox(height: context.highHeightPadding),
-        ],
+  AppBar appBarCustom(BuildContext context) {
+    return AppBar(
+      elevation: 5,
+      title: Image(
+        image: const AssetImage(
+          'assets/images/marvel-logo.png',
+        ),
+        width: MediaQuery.of(context).size.width * 0.35,
       ),
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: context.mediumWidthPadding2),
+          child: Icon(
+            Icons.settings_outlined,
+            color: context.marvelRed,
+          ),
+        ),
+      ],
     );
+  }
 
-
-*/
+  /* BottomNavigationBar bottomNavBar() {
+    return BottomNavigationBar(
+      elevation: 5,
+      onTap: (value) {
+        bottomNavBarOnTap(value);
+      },
+      selectedIconTheme: const IconThemeData(
+        color: Color(0xffED1D24),
+        size: 30.0,
+      ),
+      unselectedIconTheme: const IconThemeData(color: Colors.white),
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.sort_outlined), label: ''),
+      ],
+    );
+  } */
+}
