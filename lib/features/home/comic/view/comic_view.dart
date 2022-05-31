440e0ce334bf9b20041707f1b9d92f5e665fbf27
UKTQT <ufukkt2699@hotmail.com>
2022-05-31 23:02:35 +0300

    comic view draggable sheet update

--- lib/features/home/comic/view/comic_view.dart
+++ lib/features/home/comic/view/comic_view.dart
@@ -19,86 +19,96 @@ class ComicView extends StatelessWidget {
       },
       onPageBuilder: (BuildContext context, ComicViewModel _comicViewModel) {
         return Scaffold(
-          body: Container(
-            height: double.maxFinite,
-            child: Stack(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      child: FittedBox(
-                        fit: BoxFit.fill,
-                        child: Hero(
-                          tag: args['comicId'],
-                          child: CachedNetworkImage(
-                              imageUrl:
-                                  args['comicPath'] + '.' + args['comicExt'],
-                              placeholder: (context, index) {
-                                return Placeholder();
-                              },
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.add)),
+          body: SafeArea(
+            child: Container(
+              height: double.maxFinite,
+              child: Stack(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: FittedBox(
+                          fit: BoxFit.fill,
+                          child: Hero(
+                            tag: args['comicId'],
+                            child: CachedNetworkImage(
+                                imageUrl:
+                                    args['comicPath'] + '.' + args['comicExt'],
+                                placeholder: (context, index) {
+                                  return Placeholder();
+                                },
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.add)),
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: context.mediumWidthPadding2,
-                            vertical: context.lowHeightPadding2,
-                          ),
-                          child: IconButton(
-                            splashColor: Colors.white,
-                            highlightColor: Colors.white,
-                            color: Colors.white,
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(
-                              Icons.arrow_back_ios,
-                              size: 25,
-                              color: Colors.black,
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: context.mediumWidthPadding2,
+                              vertical: context.lowHeightPadding2,
+                            ),
+                            child: IconButton(
+                              splashColor: Colors.white,
+                              highlightColor: Colors.white,
+                              color: Colors.white,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(
+                                Icons.arrow_back_ios,
+                                size: 25,
+                                color: Colors.black,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                DraggableScrollableSheet(
-                  initialChildSize: 0.1,
-                  minChildSize: 0.1,
-                  maxChildSize: 0.5,
-                  builder: (context, scrollController) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20.0),
-                          topRight: Radius.circular(20.0),
-                        ),
+                        ],
                       ),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  'Detay',
-                                  style: TextStyle(color: Colors.black),
-                                )
-                              ],
-                            )
-                          ],
+                    ],
+                  ),
+                  DraggableScrollableSheet(
+                    initialChildSize: 0.1,
+                    minChildSize: 0.1,
+                    maxChildSize: 0.6,
+                    snap: true,
+                    snapSizes: [.6],
+                    builder: (context, scrollController) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30.0),
+                            topRight: Radius.circular(30.0),
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                        child: SingleChildScrollView(
+                          controller: scrollController,
+                          child: Column(
+                            children: [
+                              SizedBox(height: context.lowHeightPadding),
+                              Center(
+                                child: Icon(
+                                  Icons.bakery_dining,
+                                  size: 40.0,
+                                  color: Color(0xff121212),
+                                ),
+                              ),
+                              SizedBox(height: context.mediumHeightPadding),
+                              Text(
+                                'Ewq',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         );
